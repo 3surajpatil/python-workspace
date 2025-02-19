c237fa77c8e581456970d763542cd96ca242c5c2
3surajpatil <3surajpatil@gmail.com>
2025-02-19 17:24:52 +0530

    updates

--- flask-app2/flask_app2_service.py
+++ flask-app2/flask_app2_service.py
@@ -9,7 +9,7 @@ cors = CORS(app)
 @app.route('/hello')
 @cross_origin()
 def helloWorld():
-    return "hello world"
+    return "hello world..! i am from flask_app2_service "
 
 if __name__ == '__main__':   
-    app.run(host='localhost', port=6152)
+    app.run(host='0.0.0.0', port=6153)
---
+++ practice/funDemo.py
@@ -0,0 +1,8 @@
+def funTest(x):
+    print('x inside function is {0}',x)
+    x=20
+    print('x inside function is {0}',x)
+    
+x=10
+funTest(x)
+print(x)
